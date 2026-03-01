73fbcbb4d35527d6e9ad5a9f3eb1ea247c0d5dff
srikarrao2003 <srikarraonaropanth@gmail.com>
2026-03-01 16:56:05 +0000

    chore: added autosuggestions to copilot plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -21,15 +21,15 @@ copilot.setup({
       },
       suggestion = {
         enabled = true,
-        auto_trigger = false,
+        auto_trigger = true,
         hide_during_completion = true,
         debounce = 75,
         trigger_on_accept = true,
         keymap = {
-          accept = "<M-f>",
+          accept = "<M-i>",
           accept_word = false,
-          accept_line = false,
-          next = "<M-l>",
+          accept_line = "<M-l>",
+          next = "<M-n>",
         },
       },
       filetypes = {
